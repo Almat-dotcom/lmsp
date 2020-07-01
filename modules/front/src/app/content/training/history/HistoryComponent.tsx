d5b65db214aa31a95f37166bdb02330c14fafb64
maxim.sherstoboyev <maxim.sherstoboyev@uco.kz>
2020-07-01 12:03:35 +0600

    add history component

---
+++ modules/front/src/app/content/training/history/HistoryComponent.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Content from "../../Content";
+import {action, observable} from "mobx";
+import {observer} from "mobx-react";
+import CourseComponent, {CourseType} from "../../../common/CourseComponent/CourseComponent";
+import {Course} from "../../../../cuba/entities/tsadv/tsadv$Course";
+import {restServices} from "../../../../cuba/services";
+import {getCubaREST} from "@cuba-platform/react";
+import LoadingComponent from "../../../common/loading/LoadingComponent";
+
+@observer
+class HistoryComponent extends React.Component {
+
+  @observable private historyCourses: Course[];
+
+  componentDidMount(): void {
+    restServices.tsadv_LmsService.getPersonHistory(getCubaREST()!)().then((response: string) => {
+      const courses: Course[] = JSON.parse(response);
+      this.setHistoryCourses(courses);
+    })
+  }
+
+  @action setHistoryCourses = (value: Course[]) => {
+    this.historyCourses = value;
+  };
+
+  render() {
+    const HistoryBody = (historyCourses: Course[]) => () => {
+      return <div>{historyCourses ?
+        <CourseComponent courseType={CourseType.NO_BUTTON} courses={historyCourses}/> : <LoadingComponent/>}</div>
+    };
+
+    const ContentComponent = Content(HistoryBody(this.historyCourses));
+    return <ContentComponent headerName={"История"}/>
+  }
+}
+
+export default HistoryComponent;
--- modules/front/src/menu.ts
+++ modules/front/src/menu.ts
@@ -1,5 +1,6 @@
 import Courses from "./app/content/knowledge/courses/Courses";
 import CurrentCourses from "./app/content/training/current/CurrentCourses";
+import HistoryComponent from "./app/content/training/history/HistoryComponent";
 
 export interface IMenuItem {
   id: string,
@@ -16,53 +17,91 @@ topMenu.push({
   id: 'myTraining',
   caption: 'myTraining',
   path: '/training',
-  items: [{id: 'current', caption: 'current', path: '/training/current', component: CurrentCourses}, {
-    id: 'history',
-    caption: 'history',
-    path: '/training/history'
-  }, {
-    id: 'tests',
-    caption: 'tests',
-    disabled: true,
-    path: '/training/tests'
-  }]
+  items: [
+    {
+      id: 'current',
+      caption: 'current',
+      path: '/training/current',
+      component: CurrentCourses
+    },
+    {
+      id: 'history',
+      caption: 'history',
+      path: '/training/history',
+      component: HistoryComponent
+    }, {
+      id: 'tests',
+      caption: 'tests',
+      disabled: true,
+      path: '/training/tests'
+    }]
 });
 
 topMenu.push({
   id: 'calendar',
   caption: 'calendar',
   path: '/calendar',
-  items: [{id: 'open', caption: 'open', disabled: true, path: '/calendar/open'}]
+  items: [
+    {
+      id: 'open',
+      caption: 'open',
+      disabled: true,
+      path: '/calendar/open'
+    }
+  ]
 });
 
 topMenu.push({
   id: 'knowledgeBase',
   caption: 'knowledgeBase',
   path: '/knowledge',
-  items: [{id: 'courses', caption: 'courses', path: '/knowledge/courses', component: Courses}, {
-    id: 'books',
-    caption: 'books',
-    disabled: true,
-    path: '/knowledge/books'
-  }, {
-    id: 'articles',
-    caption: 'articles',
-    disabled: true,
-    path: '/knowledge/articles'
-  }, {id: 'videos', caption: 'videos', disabled: true, path: '/knowledge/videos'}]
+  items: [
+    {
+      id: 'courses',
+      caption: 'courses',
+      path: '/knowledge/courses',
+      component: Courses
+    },
+    {
+      id: 'books',
+      caption: 'books',
+      disabled: true,
+      path: '/knowledge/books'
+    }, {
+      id: 'articles',
+      caption: 'articles',
+      disabled: true,
+      path: '/knowledge/articles'
+    },
+    {
+      id: 'videos',
+      caption: 'videos',
+      disabled: true,
+      path: '/knowledge/videos'
+    }]
 });
 
 topMenu.push({
   id: 'myPage',
   caption: 'myPage',
   path: '/me',
-  items: [{id: 'notifications', caption: 'notifications', disabled: true, path: '/me/notifications'}, {
-    id: 'password',
-    caption: 'password',
-    disabled: true, path: '/me/password'
-  }, {
-    id: 'progress',
-    caption: 'progress',
-    disabled: true, path: '/me/progress'
-  }]
+  items: [
+    {
+      id: 'notifications',
+      caption: 'notifications',
+      disabled: true,
+      path: '/me/notifications'
+    },
+    {
+      id: 'password',
+      caption: 'password',
+      disabled: true,
+      path: '/me/password'
+    },
+    {
+      id: 'progress',
+      caption: 'progress',
+      disabled: true,
+      path: '/me/progress'
+    }]
 });
